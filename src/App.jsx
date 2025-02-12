1a882d16809983a723c251ab1cbcef6a881600e8
Himanshu <himanshu290304@gmail.com>
2025-02-13 00:16:24 +0530

    will fix bugs now

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import SeedPhraseGenerator from './components/SeedPhraseGenerator'
+import SeedPhraseWallets from './components/SeedPhraseWallets'
 function App() {
   return (
     <div>
      <h2>Welcom to the Web wallet</h2>
-     <SeedPhraseGenerator />
+     
+     <SeedPhraseWallets />
     </div>
   )
 }
